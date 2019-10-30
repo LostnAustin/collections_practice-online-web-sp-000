9503ea6ab8d90db92de58c07e609fd519ae47920
Chad Lengefeld <chadlengefeld@gmail.com>
2019-10-30 16:08:39 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -53,7 +53,7 @@ def add_s(array)
   array.each_with_index.collect do | word, index |
 
     if word[index] == index[1]
-      word 
+      word
     elsif word[index] == !index[1]
       word << "s"
   end
