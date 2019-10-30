36f4035d41602954c5523145e5ed1d293f811767
Chad Lengefeld <chadlengefeld@gmail.com>
2019-10-30 16:22:07 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -55,7 +55,7 @@ def add_s(array)
      word[index] == index[0]
    word[index] == index[2]
     word[index] == index[3]
-  return  word << "s"
+  return  word[0,2,3] << "s"
    word[index] == index[1]
   return  word
 
